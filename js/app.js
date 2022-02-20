9a319cee44f89dd8e3dfca974f02dd2514818003
morshidulrahman <76879057+morshidulrahman@users.noreply.github.com>
2022-02-21 02:18:05 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const nav=document.querySelector(".nav_menu")
+const menubtn=document.querySelector(".menu_icon")
+menubtn.addEventListener("click",()=>{
+     nav.classList.toggle("nav_active")
+})
